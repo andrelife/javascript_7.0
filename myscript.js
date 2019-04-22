c95bdbc85dd22629313605895e4f034fd92f49db
Andrei <andrelife2104@gmail.com>
2019-04-22 23:38:31 +0200

    корректировка

--- myscript.js
+++ myscript.js
@@ -1,35 +1,24 @@
-const money = prompt('Ваш бюджет на месяц?');
-const time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = prompt('Ваш бюджет на месяц?'),
+    time = prompt('Введите дату в формате YYYY-MM-DD'),
+    question1 = prompt( 'Введите обязательную статью расходов в этом месяце ?'),
+    question2 = prompt( 'Во сколько обойдется ?'),
+    question3 = prompt( 'Введите обязательную статью расходов в этом месяце ?'),
+    question4 = prompt( 'Во сколько обойдется ?');
+
 let appData = {
     
     budget: money,
     timeData : time,
-    
-    expenses: {
-        
-    },
-
+    expenses: {},
     optionalExpenses: {},
-
     income: [],
-
     savings: false
 };
 
-
-let question1 = prompt( 'Введите обязательную статью расходов в этом месяце ?');
-let question2 = prompt( 'Во сколько обойдется ?');
-
-
-    appData.expenses[question1] = question2;
-
-    let question3 = prompt( 'Введите обязательную статью расходов в этом месяце ?');
-    let question4 = prompt( 'Во сколько обойдется ?');
-
-    appData.expenses[question3] = question4;
-
-
+appData.expenses[question1] = question2;
+appData.expenses[question3] = question4;
 
 console.log(appData);
-console.log(money/30);
+console.log(appData.budget / 30);
+alert(appData.budget / 30);
 
