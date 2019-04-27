aa484c55fcbb55f1c3a0676b8afe7255110a8713
Andrei <andrelife2104@gmail.com>
2019-04-27 21:53:14 +0200

    Урок 4

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,34 +1,3 @@
-const money = prompt('Ваш бюджет на месяц?');
-const time = prompt('Введите дату в формате YYYY-MM-DD');
-let appData = {
-    
-    budget: money,
-    timeData : time,
-    
-    expenses: {
-        
-    },
-
-    optionalExpenses: {},
-
-    income: [],
-
-    savings: false
-};
-
-
-let question1 = prompt( 'Введите обязательную статью расходов в этом месяце ?');
-let question2 = prompt( 'Во сколько обойдется ?');
-
-
-    appData.expenses[question1] = question2;
-
-    let question3 = prompt( 'Введите обязательную статью расходов в этом месяце ?');
-    let question4 = prompt( 'Во сколько обойдется ?');
-
-    appData.expenses[question3] = question4;
-
-
-
-console.log(appData);
-console.log(money/30);
+for(let key in appData){
+    console.log("Наша программа включает в себя данные: " + key+ ": " + appData[key]);
+}
