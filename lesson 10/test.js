8637be80c0ae7857c30b883943248450c5219788
Andrei <andrelife2104@gmail.com>
2019-05-07 22:34:19 +0200

    Урок 10 готово!

--- lesson 10/test.js
+++ lesson 10/test.js
@@ -8,10 +8,15 @@ class Options {
     }
     createDiv() {
         let div = document.createElement('div');
-        div.innerText = 'я новый блок';
-        div.style.cssText = 'color: red; font-size: 20px;';
+        div.innerHTML = '<h4><b><i>я новый блок</i></b></h4>';
+        div.style.cssText = `height: ${this.height}px;
+                            width: ${this.width}px;
+                            background-color: ${this.bg};
+                            font-size: ${this.fontSize}px;
+                            text-align: ${this.textAlign};`;
+        document.body.appendChild(div);
     }
     
 }
-const page = new Options(25,25,'red',16,'left');
+const page = new Options(25,1024,'blue',16,'left');
 page.createDiv();
