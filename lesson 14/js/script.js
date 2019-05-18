dc8838dcb37759889293556c8b26da746662d782
Andrei <andrelife2104@gmail.com>
2019-05-18 23:30:03 +0200

    Урок 14 C/P

--- lesson 14/js/script.js
+++ lesson 14/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $('.main_btna').on('click', function(){
-        $('.overlay').fadeTo('slow');
+        $('.overlay').fadeIn('slow');
         $('.modal').slideDown(1000);
     });
     $('.main_btn').on('click', function(){
-        $('.overlay').fadeTo('slow');
+        $('.overlay').fadeIn('slow');
         $('.modal').slideDown(1000);
     });
     $('ul:eq(2) li:eq(1)').on('click', function(){
-        $('.overlay').fadeTo('slow');
+        $('.overlay').fadeIn('slow');
         $('.modal').slideDown(1000);
     });
     $('.close').on('click', function(){
